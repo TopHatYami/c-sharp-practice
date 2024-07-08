2609cd8afd8a995ba68bc977cebc86be8fa54d03
Tom Pearson <tophatyami@gmail.com>
2024-07-08 11:58:02 -0600

    Create Model and Controller

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add DbContext with PostgreSQL provider
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("MyDbConnection")));
 
 var app = builder.Build();
 
